570c3bc44f57fdcfccf5fa22f97fc042f3f23681
tImagawa09 <shiliusuaaa@gmail.com>
2023-07-10 15:50:24 +0900

    JSデータ構造途中

--- 02_webサイトに動きを付けてみよう/03_JavaScript入門 データ構造編/main.js
+++ 02_webサイトに動きを付けてみよう/03_JavaScript入門 データ構造編/main.js
@@ -1,5 +1,17 @@
 'use sprict';
 
 {
+    const scores = [70, 90, 80, 85];
+
+    scores.push(77, 88)
+
+    for (let i=0; i<scores.length; i++) {
+        console.log(scores[i]);
+    }
+
+    scores.forEach((score, IDBIndex) => {
+        console.log(`${index}: ${score}`);
+    })
+
 
 }
