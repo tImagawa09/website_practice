6070ec75d67b1b46abf00576b34338c6a9b4d65d
tImagawa09 <shiliusuaaa@gmail.com>
2023-07-13 10:24:25 +0900

    データ型終了

--- 02_webサイトに動きを付けてみよう/09_JavaScript入門 データ型編/main.js
+++ 02_webサイトに動きを付けてみよう/09_JavaScript入門 データ型編/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
 {
-    
+    let isDevMode = true;
+    const pElement = document.querySelector('p');
+    const buttonElement = document.querySelector('button');
+
+    buttonElement.addEventListener('click', () => {
+        if (isDevMode === true) {
+            pElement.textContent = 'Dev Mode is Off';
+            // isDevMode = false; 
+            isDevMode = !isDevMode
+        } else {
+            pElement.textContent = 'Dev Mode is ON';
+            // isDevMode = true;
+        }
+        isDevMode = !isDevMode;
+    })
 }
