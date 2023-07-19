3b6e5317e85ab0a81fa23b58b2eebf1e8eda1627
tImagawa09 <shiliusuaaa@gmail.com>
2023-07-20 08:14:22 +0900

    途中

---
+++ 03_JavaScriptでミニアプリを作ってみよう/02_JacaScriptでパスワードジェネレーターを作ろう/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+{
+    const slider = document.getElementById('slider');
+    const btn = document.getElementById('btn');
+
+
+    slider.addEventListener('input', () => {
+        const passwordLength = document.getElementById('password-length');
+
+        passwordLength.textContent = slider.value;
+    })
+
+
+    btn.addEventListener('click', () => {
+        const result = document.getElementById('result');
+        const numberCheckbox = document.getElementById('numbers-checkbox');
+        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        const numbers = '0123456789';
+        let password = '';
+        let seed = letters + letters.toUpperCase();
+
+        if (numberCheckbox.checked) {
+            seed += numbers;
+        }
+
+
+        for (let i = 0; i < slider.value; i++) {
+            password += seed[Math.floor(Math.random() * seed.length)];
+        }
+
+        result.textContent = password;
+    });
+}
