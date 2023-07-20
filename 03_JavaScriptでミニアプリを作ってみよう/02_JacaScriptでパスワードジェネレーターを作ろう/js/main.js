7ffe8c6417fd4340ecd8fb0a2c082756c9f9dd6e
timagawa09 <shiliusuaaa@gmail.com>
2023-07-20 21:48:09 +0900

    終了

--- 03_JavaScriptでミニアプリを作ってみよう/02_JacaScriptでパスワードジェネレーターを作ろう/js/main.js
+++ 03_JavaScriptでミニアプリを作ってみよう/02_JacaScriptでパスワードジェネレーターを作ろう/js/main.js
@@ -1,5 +1,33 @@
 'use strict';
 {
+    function showPassword() {
+        const result = document.getElementById('result');
+        const numberCheckbox = document.getElementById('numbers-checkbox');
+        const symbolCheckbox = document.getElementById('symbols-checkbox');
+        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        const numbers = '0123456789';
+        const symbols = '!#$%&()'
+        let password = '';
+        let seed = letters + letters.toUpperCase();
+
+        if (numberCheckbox.checked) {
+            seed += numbers;
+        }
+
+        if (symbolCheckbox.checked) {
+            seed += symbols;
+        }
+
+
+        for (let i = 0; i < slider.value; i++) {
+            password += seed[Math.floor(Math.random() * seed.length)];
+        }
+
+        result.textContent = password;
+    }
+
+
+
     const slider = document.getElementById('slider');
     const btn = document.getElementById('btn');
 
@@ -12,22 +40,8 @@
 
 
     btn.addEventListener('click', () => {
-        const result = document.getElementById('result');
-        const numberCheckbox = document.getElementById('numbers-checkbox');
-        const letters = 'abcdefghijklmnopqrstuvwxyz';
-        const numbers = '0123456789';
-        let password = '';
-        let seed = letters + letters.toUpperCase();
-
-        if (numberCheckbox.checked) {
-            seed += numbers;
-        }
-
-
-        for (let i = 0; i < slider.value; i++) {
-            password += seed[Math.floor(Math.random() * seed.length)];
-        }
-
-        result.textContent = password;
+        showPassword();
     });
+
+    showPassword();
 }
